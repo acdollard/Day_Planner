719ec322b6a579c51fdd0b4141d853beee6a3155
Alex Dollard <alexdollard141@gmail.com>
2019-11-21 10:04:21 -0600

    got save button & local storage working for all textareas
    the save button now commits the textarea value to local storage, and a separate function populates the text area values on opening the page

--- script.js
+++ script.js
@@ -22,14 +22,90 @@ $(document).ready(function() {
 
 
 
+//function to set val() of textareas by pulling from local storage 
+function resetText() {
+    $("#9amtext").val(localStorage.getItem("9amsaved_text"));
+    $("#10amtext").val(localStorage.getItem("10amsaved_text"));
+    $("#11amtext").val(localStorage.getItem("11amsaved_text"));
+    $("#12pmtext").val(localStorage.getItem("12pmsaved_text"));
+    $("#1pmtext").val(localStorage.getItem("1pmsaved_text"));
+    $("#2pmtext").val(localStorage.getItem("2pmsaved_text"));
+    $("#3pmtext").val(localStorage.getItem("3pmsaved_text"));
+    $("#4pmtext").val(localStorage.getItem("4pmsaved_text"));
+    $("#5pmtext").val(localStorage.getItem("5pmsaved_text"));
 
-$(".row").on("click", function(){
-   if(event.target.matches("button"))
-   {
-       debugger; 
-       console.log($(this.childNodes[3].innerHTML));
-   }
-});
+}
+
+resetText();
+
+
+
+
+
+//functions to save text from textarea in local storage
+    $("#9amBtn").on("click", function(){
+        let saved_text = $("#9amtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("9amsaved_text", saved_text));
+    });
+    $("#10amBtn").on("click", function(){
+        let saved_text = $("#10amtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("10amsaved_text", saved_text));
+    });
+    $("#11amBtn").on("click", function(){
+        let saved_text = $("#11amtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("11amsaved_text", saved_text));
+    });
+    $("#12pmBtn").on("click", function(){
+        let saved_text = $("#12pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("12pmsaved_text", saved_text));
+    });
+    $("#1pmBtn").on("click", function(){
+        let saved_text = $("#1pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("1pmsaved_text", saved_text));
+    });
+    $("#2pmBtn").on("click", function(){
+        let saved_text = $("#2pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("2pmsaved_text", saved_text));
+    });
+    $("#3pmBtn").on("click", function(){
+        let saved_text = $("#3pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("3pmsaved_text", saved_text));
+    });
+    $("#4pmBtn").on("click", function(){
+        let saved_text = $("#4pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("4pmsaved_text", saved_text));
+    });
+    $("#5pmBtn").on("click", function(){
+        let saved_text = $("#5pmtext").val();
+        console.log(saved_text);
+        JSON.stringify(localStorage.setItem("5pmsaved_text", saved_text));
+    });
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
