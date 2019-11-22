f10247bdd3b761ed203381c0a98bed18c1a366ce
Alex Dollard <alexdollard141@gmail.com>
2019-11-22 10:16:07 -0600

    condensed local storage functions down into one function

--- script.js
+++ script.js
@@ -23,83 +23,96 @@ $("#currentDay").text("Today's date: " + today);
 
     // console.log(moment(new Date()));
     
-
+$(".saveBtn").on("click", function(event) {
+    console.log(event.target.id);
+    btnID = event.target.id; 
+    text = $("#" + btnID + "text").val();
+    localStorage.setItem(btnID, text);
+})
 
 
 //function to set val() of textareas by pulling from local storage 
 function resetText() {
-    $("#9amtext").val(localStorage.getItem("9amsaved_text"));
-    $("#10amtext").val(localStorage.getItem("10amsaved_text"));
-    $("#11amtext").val(localStorage.getItem("11amsaved_text"));
-    $("#12pmtext").val(localStorage.getItem("12pmsaved_text"));
-    $("#1pmtext").val(localStorage.getItem("1pmsaved_text"));
-    $("#2pmtext").val(localStorage.getItem("2pmsaved_text"));
-    $("#3pmtext").val(localStorage.getItem("3pmsaved_text"));
-    $("#4pmtext").val(localStorage.getItem("4pmsaved_text"));
-    $("#5pmtext").val(localStorage.getItem("5pmsaved_text"));
+    $("#9amtext").val(localStorage.getItem("9am"));
+    $("#10amtext").val(localStorage.getItem("10am"));
+    $("#11amtext").val(localStorage.getItem("11am"));
+    $("#12pmtext").val(localStorage.getItem("12pm"));
+    $("#1pmtext").val(localStorage.getItem("1pm"));
+    $("#2pmtext").val(localStorage.getItem("2pm"));
+    $("#3pmtext").val(localStorage.getItem("3pm"));
+    $("#4pmtext").val(localStorage.getItem("4pm"));
+    $("#5pmtext").val(localStorage.getItem("5pm"));
 
 }
 resetText();
 
 //functions to save text from textarea in local storage
-    $("#9amBtn").on("click", function(){
-        let saved_text = $("#9amtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("9amsaved_text", saved_text));
-    });
-    $("#10amBtn").on("click", function(){
-        let saved_text = $("#10amtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("10amsaved_text", saved_text));
-    });
-    $("#11amBtn").on("click", function(){
-        let saved_text = $("#11amtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("11amsaved_text", saved_text));
-    });
-    $("#12pmBtn").on("click", function(){
-        let saved_text = $("#12pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("12pmsaved_text", saved_text));
-    });
-    $("#1pmBtn").on("click", function(){
-        let saved_text = $("#1pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("1pmsaved_text", saved_text));
-    });
-    $("#2pmBtn").on("click", function(){
-        let saved_text = $("#2pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("2pmsaved_text", saved_text));
-    });
-    $("#3pmBtn").on("click", function(){
-        let saved_text = $("#3pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("3pmsaved_text", saved_text));
-    });
-    $("#4pmBtn").on("click", function(){
-        let saved_text = $("#4pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("4pmsaved_text", saved_text));
-    });
-    $("#5pmBtn").on("click", function(){
-        let saved_text = $("#5pmtext").val();
-        console.log(saved_text);
-        JSON.stringify(localStorage.setItem("5pmsaved_text", saved_text));
-    });
+    // $("#9amBtn").on("click", function(){
+    //     let saved_text = $("#9amtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("9amsaved_text", saved_text));
+    // });
+    // $("#10amBtn").on("click", function(){
+    //     console.log(event);
+    //     let saved_text = $("#10amtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("10amsaved_text", saved_text));
+    // });
+    // $("#11amBtn").on("click", function(){
+    //     let saved_text = $("#11amtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("11amsaved_text", saved_text));
+    // });
+    // $("#12pmBtn").on("click", function(){
+    //     let saved_text = $("#12pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("12pmsaved_text", saved_text));
+    // });
+    // $("#1pmBtn").on("click", function(){
+    //     let saved_text = $("#1pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("1pmsaved_text", saved_text));
+    // });
+    // $("#2pmBtn").on("click", function(){
+    //     let saved_text = $("#2pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("2pmsaved_text", saved_text));
+    // });
+    // $("#3pmBtn").on("click", function(){
+    //     let saved_text = $("#3pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("3pmsaved_text", saved_text));
+    // });
+    // $("#4pmBtn").on("click", function(){
+    //     let saved_text = $("#4pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("4pmsaved_text", saved_text));
+    // });
+    // $("#5pmBtn").on("click", function(){
+    //     let saved_text = $("#5pmtext").val();
+    //     console.log(saved_text);
+    //     JSON.stringify(localStorage.setItem("5pmsaved_text", saved_text));
+    // });
+
+
+    // if(moment().hour().isBefore($())
         
 
     if (moment().isBefore(moment(today + " 09:00"))){
         $("#9amtext").css({"background-color":"mediumseagreen" , "color":"white"});
     } else if(moment().isAfter(moment(today + " 09:00"))){
         $("#9amtext").css({"background-color":"tomato" , "color":"white"})
-    } ;
+    } else if(moment().hour().isSame("9")){
+        $("#9amtext").css({"background-color":"cornflowerblue" , "color":"white"})
+    };
 
-    if (moment().isBefore(moment(today + " 10:00"))){
+    if (moment().hour() < 10){
         $("#10amtext").css({"background-color":"mediumseagreen" , "color":"white"});
-    } else if(moment().isAfter(moment(today + " 10:00"))){
+    } else if(moment().hour() > 10){
         $("#10amtext").css({"background-color":"tomato" , "color":"white"})
-    } ;
+    } else if(moment().hour() == 10){
+        $("#10amtext").css({"background-color":"cornflowerblue" , "color":"white"})
+    };
 
     if (moment().isBefore(moment(today + " 11:00"))){
         $("#11amtext").css({"background-color":"mediumseagreen" , "color":"white"});
